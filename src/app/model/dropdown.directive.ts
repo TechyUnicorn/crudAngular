7ee983702211a7abc06409a160dfaebb2dcc7609
Abhiram Ojaswi <aojaswi@digitalharbor.com>
2020-03-03 12:23:32 +0530

    Added dropdown directive

---
+++ src/app/model/dropdown.directive.ts
@@ -0,0 +1,18 @@
+import {
+  Directive,
+  ElementRef,
+  Renderer2,
+  HostListener,
+  HostBinding
+} from "@angular/core";
+
+@Directive({
+  selector: "[appDropdown]"
+})
+export class DropdownDirectives {
+  @HostBinding("class.open") isOpen = false;
+
+  @HostListener("click") toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
+}
